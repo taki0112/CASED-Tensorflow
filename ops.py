0d5a4e2bcbf90437de11677db8059f7b6e455c45
Junho Kim <takis0112@gmail.com>
2017-12-04 18:10:53 +0900

    add he_init

--- ops.py
+++ ops.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
-
+from tensorflow.contrib.layers import variance_scaling_initializer as he_init
 def conv_layer(x, channels, kernel=3, stride=1, activation='relu', padding='VALID', layer_name='_conv3d') :
     with tf.name_scope(layer_name) :
         if activation == None :
-            return tf.layers.conv3d(inputs=x, filters=channels, kernel_size=kernel, strides=stride, padding=padding)
+            return tf.layers.conv3d(inputs=x, filters=channels, kernel_size=kernel, kernel_initializer=he_init(), strides=stride, padding=padding)
         else :
-            return tf.layers.conv3d(inputs=x, filters=channels, kernel_size=kernel, strides=stride, padding=padding, activation=relu)
+            return tf.layers.conv3d(inputs=x, filters=channels, kernel_size=kernel, kernel_initializer=he_init(), strides=stride, padding=padding, activation=relu)
 
 def deconv_layer(x, channels, kernel=4, stride=2, padding='VALID', layer_name='_deconv3d') :
     with tf.name_scope(layer_name) :
-        x = tf.layers.conv3d_transpose(inputs=x, filters=channels, kernel_size=kernel, strides=stride, padding=padding)
+        x = tf.layers.conv3d_transpose(inputs=x, filters=channels, kernel_size=kernel, kernel_initializer=he_init(), strides=stride, padding=padding)
         x = x[:, 1:-1, 1:-1, 1:-1, :]
         return x
 def flatten(x) :
@@ -17,7 +17,7 @@ def flatten(x) :
 
 def fully_connected(x, unit=2, layer_name='fully') :
     with tf.name_scope(layer_name) :
-        return tf.layers.dense(x, units=unit)
+        return tf.layers.dense(x, units=unit, kernel_initializer=he_init())
 
 def max_pooling(x, kernel=2, stride=2, padding='VALID') :
     x = tf.concat([x, x], axis=-1)
