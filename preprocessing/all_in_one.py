8539362280ffcd1e520b0c66ca55871e29451744
Junho Kim <takis0112@gmail.com>
2017-12-08 13:43:53 +0900

    preprocessing

---
+++ preprocessing/all_in_one.py
@@ -0,0 +1,162 @@
+from preprocessing.preprocess_utils import *
+from random import randint
+import h5py
+OUTPUT_SPACING = [1, 1, 1]
+patch_size = 68
+label_size = 8
+stride = 8
+move = (patch_size // 2) // stride  # 4
+
+
+if __name__=="__main__":
+
+    from matplotlib.patches import Circle
+
+    coord = False
+
+    dst_spacing = OUTPUT_SPACING
+    data_root = '/data/jhkim/LUNA16'
+    src_root = '/data/jhkim/LUNA16/original'
+    #dst_root = '/lunit/data/LUNA16/npydata'
+
+    save_path = '/data2/jhkim/LUNA16/patch/SH/'
+
+    # if not os.path.exists(dst_root):
+    #     os.makedirs(dst_root)
+
+    annotation_csv = os.path.join(data_root,'CSVFILES/annotations.csv')
+    annotations = read_csv(annotation_csv)
+
+    src_mhd = []
+    coord_dict = {}
+    for sub_n in range(10) :
+        idx = 1
+        sub_n_str = 'subset' + str(sub_n)
+        image_paths = glob.glob(os.path.join(src_root,sub_n_str,'*.mhd'))
+
+        nodule = []
+        non_nodule = []
+        nodule_label = []
+        non_nodule_label = []
+
+        for i in image_paths:
+            filename = os.path.split(i)[-1]
+            series_uid = os.path.splitext(filename)[0]
+
+            subset_num = i.split('/')[-2]
+            # dst_subset_path = os.path.join(dst_root,subset_num)
+            #
+            # if not os.path.exists(dst_subset_path):
+            #     os.makedirs(dst_subset_path)
+
+            np_img, origin, spacing = load_itk_image(i)
+
+            resampled_img, new_spacing = resample(np_img, spacing, dst_spacing)
+            resampled_img_shape = resampled_img.shape
+
+            norm_img = normalize_planes(resampled_img)
+            norm_img = zero_center(norm_img)
+
+            nodule_coords = []
+            try:
+                nodule_coords = annotations[series_uid]
+                label = create_label(resampled_img_shape, nodule_coords, origin, new_spacing, coord=coord)
+            except:
+                if coord:
+                    label = []
+                else:
+                    label = np.zeros(resampled_img_shape, dtype='bool')
+
+            image = np.transpose(norm_img)
+            label = np.transpose(label)
+
+            #np.save(os.path.join(dst_subset_path,series_uid+'.npy'), norm_img)
+            #np.save(os.path.join(dst_subset_path,series_uid+'.label.npy'), label)
+            #coord_dict[os.path.join(dst_subset_path,series_uid+'.npy')] = label
+
+            # padding
+            offset = patch_size // 2
+
+            non_pad = image
+            non_label_pad = label
+
+            non_pad = np.pad(non_pad, offset, 'constant', constant_values=0)
+            non_label_pad = np.pad(non_label_pad, offset, 'constant', constant_values=0)
+
+            image = np.pad(image, offset + (stride * move), 'constant', constant_values=0)
+            label = np.pad(label, offset + (stride * move), 'constant', constant_values=0)
+
+            nodule_list = []
+            nodule_label_list = []
+
+            for nodule_coord in nodule_coords :
+                worldCoord = nodule_coord['position']
+                worldCoord = np.asarray([worldCoord[0], worldCoord[1], worldCoord[2]])
+
+                # new_spacing came from resample
+                voxelCoord = compute_coord(worldCoord, origin, new_spacing)
+                voxelCoord = [int(i) + offset + (stride * move) for i in voxelCoord]
+
+                patch_stride(image, nodule_coord, offset, nodule_list)  # x,y,z, xy,xz,yz, xyz ... get stride patch
+                patch_stride(label, nodule_coord, offset, nodule_label_list, patch_flag=False)
+
+            #print(series_uid)
+
+            nodule_num = len(nodule_list)
+
+            non_nodule_list = []
+            non_nodule_label_list = []
+            x_coords = non_pad.shape[0] - offset - 1
+            y_coords = non_pad.shape[1] - offset - 1
+            z_coords = non_pad.shape[2] - offset - 1
+
+            while len(non_nodule_list) < 3 * nodule_num:
+                rand_x = randint(offset, x_coords)
+                rand_y = randint(offset, y_coords)
+                rand_z = randint(offset, z_coords)
+
+                coords = np.array([rand_x, rand_y, rand_z])
+
+                get_patch(non_pad, coords, offset, non_nodule_list)
+                get_patch(non_label_pad, coords, offset, non_nodule_label_list, patch_flag=False)
+
+            nodule.extend(nodule_list)
+            non_nodule.extend(non_nodule_list)
+            nodule_label.extend(nodule_label_list)
+            non_nodule_label.extend(non_nodule_label_list)
+
+            print(sub_n_str + ' / ' + str(idx) + ' / ' + str(len(image_paths)))
+            print('nodule : ', np.shape(nodule))
+            print('nodule_label : ', np.shape(nodule_label))
+            print('non-nodule : ', np.shape(non_nodule))
+            print('non-nodule_label : ', np.shape(non_nodule_label))
+            idx += 1
+
+        np.random.seed(0)
+        np.random.shuffle(nodule)
+        np.random.seed(0)
+        np.random.shuffle(nodule_label)
+
+        np.random.seed(0)
+        np.random.shuffle(non_nodule)
+        np.random.seed(0)
+        np.random.shuffle(non_nodule_label)
+
+        train_nodule_len = int(len(nodule) * 0.7)
+        train_non_nodule_len = int(len(non_nodule) * 0.7)
+
+        with h5py.File(save_path + 'subset' + str(sub_n) + '.h5', 'w') as hf:
+            hf.create_dataset('nodule', data=nodule[:train_nodule_len], compression='lzf')
+            hf.create_dataset('label_nodule', data=nodule_label[:train_nodule_len], compression='lzf')
+
+            hf.create_dataset('non_nodule', data=non_nodule[:train_non_nodule_len], compression='lzf')
+            hf.create_dataset('label_non_nodule', data=non_nodule_label[:train_non_nodule_len], compression='lzf')
+
+        with h5py.File(save_path + 't_subset' + str(sub_n) + '.h5', 'w') as hf:
+            hf.create_dataset('nodule', data=nodule[train_nodule_len:], compression='lzf')
+            hf.create_dataset('label_nodule', data=nodule_label[train_nodule_len:], compression='lzf')
+
+            hf.create_dataset('non_nodule', data=non_nodule[train_non_nodule_len:], compression='lzf')
+            hf.create_dataset('label_non_nodule', data=non_nodule_label[train_non_nodule_len:], compression='lzf')
+
+
---
+++ preprocessing/preprocess_utils.py
@@ -0,0 +1,247 @@
+import SimpleITK as sitk
+import numpy as np
+import csv
+import os, glob
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import scipy.ndimage
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from skimage.measure import block_reduce
+
+OUTPUT_SPACING = [1, 1, 1]
+patch_size = 68
+label_size = 8
+stride = 8
+
+move = (patch_size // 2) // stride  # 4
+
+def load_itk_image(filename):
+    itkimage = sitk.ReadImage(filename)
+    numpyImage = sitk.GetArrayFromImage(itkimage)
+
+    numpyOrigin = np.array(list(reversed(itkimage.GetOrigin())))
+    numpySpacing = np.array(list(reversed(itkimage.GetSpacing())))
+
+    return numpyImage, numpyOrigin, numpySpacing
+
+def read_csv(filename):
+    lines = []
+    with open(filename, 'rb') as f:
+        csvreader = csv.reader(f)
+        for line in csvreader:
+            lines.append(line)
+
+    lines = lines[1:] # remove csv headers
+    annotations_dict = {}
+    for i in lines:
+        series_uid, x, y, z, diameter = i
+        value = {'position':[float(x),float(y),float(z)],
+                 'diameter':float(diameter)}
+        if series_uid in annotations_dict.keys():
+            annotations_dict[series_uid].append(value)
+        else:
+            annotations_dict[series_uid] = [value]
+
+    return annotations_dict
+
+def compute_coord(worldCoord, origin, spacing):
+    stretchedVoxelCoord = np.absolute(worldCoord - origin)
+    voxelCoord = stretchedVoxelCoord / spacing
+    return voxelCoord
+
+def normalize_planes(npzarray):
+    maxHU = 600.
+    minHU = -1200.
+
+    npzarray = (npzarray - minHU) / (maxHU - minHU)
+    npzarray[npzarray>1] = 1.
+    npzarray[npzarray<0] = 0.
+    return npzarray
+
+def zero_center(image):
+    PIXEL_MEAN = 0.25
+    image = image - PIXEL_MEAN
+
+    return image
+
+def indices_to_one_hot(data, nb_classes):
+    """Convert an iterable of indices to one-hot encoded labels."""
+    targets = np.array(data).reshape(-1)
+    return np.eye(nb_classes)[targets]
+
+def get_patch(image, coords, offset, nodule_list, patch_flag=True):
+    xyz = image[int(coords[0] - offset): int(coords[0] + offset), int(coords[1] - offset): int(coords[1] + offset),
+          int(coords[2] - offset): int(coords[2] + offset)]
+
+    if patch_flag:
+        output = np.expand_dims(xyz, axis=-1)
+    else:
+        # resize xyz
+        """
+        xyz = scipy.ndimage.zoom(input=xyz, zoom=1/8, order=1) # nearest
+        xyz = np.where(xyz > 0, 1.0, 0.0)
+        """
+        xyz = block_reduce(xyz, (9, 9, 9), np.max)
+        output = np.expand_dims(xyz, axis=-1)
+
+        output = indices_to_one_hot(output.astype(np.int32), 2)
+        output = np.reshape(output, (label_size, label_size, label_size, 2))
+        output = output.astype(np.float32)
+    nodule_list.append(output)
+
+def patch_stride(image, coords, offset, nodule_list, patch_flag=True):
+    # get stride * 7
+    # In this case, get 8*7 = 56
+    original_coords = coords
+    move_stride = stride
+
+    # center
+    get_patch(image, coords, offset, nodule_list, patch_flag)
+
+    # x
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords[0] = coords[0] + move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords[0] = coords[0] - move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+    # y
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords[1] = coords[1] + move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords[1] = coords[1] - move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+    # z
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords[2] = coords[2] + move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords[2] = coords[2] - move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+    # xy
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords[0] = coords[0] + move_stride
+        coords[1] = coords[1] + move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords[0] = coords[0] - move_stride
+        coords[1] = coords[1] - move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+    # xz
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords[0] = coords[0] + move_stride
+        coords[2] = coords[2] + move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords[0] = coords[0] - move_stride
+        coords[2] = coords[2] - move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+    # yz
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords[1] = coords[1] + move_stride
+        coords[2] = coords[2] + move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords[1] = coords[1] - move_stride
+        coords[2] = coords[2] - move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+    # xyz
+    for idx in range(1, move + 1):
+        move_stride = stride * idx
+        coords += move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+        coords -= move_stride
+        get_patch(image, coords, offset, nodule_list, patch_flag)
+        coords = original_coords
+
+def resample(image, org_spacing, new_spacing):
+
+    resize_factor = org_spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = org_spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+
+    return image, new_spacing
+
+def create_label(arr_shape, nodules, origin, new_spacing, coord=False):
+    """
+        nodules = list of dict {'position', 'diameter'}
+    """
+
+    def _create_mask(arr_shape, position, diameter):
+
+        z_dim, y_dim, x_dim = arr_shape
+        z_pos, y_pos, x_pos = position
+
+        z,y,x = np.ogrid[-z_pos:z_dim-z_pos, -y_pos:y_dim-y_pos, -x_pos:x_dim-x_pos]
+        mask = z**2 + y**2 + x**2 <= int(diameter)**2
+
+        return mask
+
+    if coord:
+        label = []
+    else:
+        label = np.zeros(arr_shape, dtype='bool')
+
+    for nodule in nodules:
+        worldCoord = nodule['position']
+        worldCoord = np.asarray([worldCoord[2],worldCoord[1],worldCoord[0]])
+
+        # new_spacing came from resample
+        voxelCoord = compute_coord(worldCoord, origin, new_spacing)
+        voxelCoord = [int(i) for i in voxelCoord]
+
+        diameter = nodule['diameter']
+        diameter = diameter / new_spacing[1]
+
+        if coord:
+            label.append(voxelCoord + [diameter])
+        else:
+            mask = _create_mask(arr_shape, voxelCoord, diameter)
+            label = np.logical_or(label, mask)
+
+    return label
+
+
+def plot(image, label, z_idx):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,2,1)
+    ax.imshow(image[z_idx,:,:],cmap='gray')
+
+    ax = fig.add_subplot(1,2,2)
+    ax.imshow(label[z_idx,:,:],cmap='gray')
+
+    fig.show()
